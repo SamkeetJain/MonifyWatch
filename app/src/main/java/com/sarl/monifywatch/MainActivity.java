b665fb56c71689f03d7bf21dfb8f356f15e2db52
SamkeetJain <jain.sankeet2210@gmail.com>
2017-11-03 13:18:07 +0530

    added lineonecard

--- app/src/main/java/com/sarl/monifywatch/MainActivity.java
+++ app/src/main/java/com/sarl/monifywatch/MainActivity.java
@@ -1,7 +1,13 @@
 package com.sarl.monifywatch;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+
+import com.db.chart.tooltip.Tooltip;
+import com.db.chart.view.LineChartView;
+import com.sarl.monifywatch.linechart.LineCardOne;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CardView cardView = (CardView) findViewById(R.id.chart_card);
+        LineCardOne lineCardOne = new LineCardOne(cardView,getApplicationContext());
+        lineCardOne.init();
+
+
     }
 }
